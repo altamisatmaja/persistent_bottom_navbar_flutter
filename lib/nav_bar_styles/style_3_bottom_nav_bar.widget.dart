348b708f0e6b94bcb7034178d65092f7e436f9c4
altamisatmaja <altamisfattah16@gmail.com>
2025-01-14 06:33:20 +0700

    fix: adjust minor

--- lib/nav_bar_styles/style_3_bottom_nav_bar.widget.dart
+++ lib/nav_bar_styles/style_3_bottom_nav_bar.widget.dart
@@ -70,75 +70,87 @@ class _BottomNavStyle3 extends StatelessWidget {
               ),  
             );  
   
-  Widget _buildMiddleItem(final PersistentBottomNavBarItem item,  
-          final bool isSelected, final double? height) =>  
-      navBarEssentials.navBarHeight == 0  
-          ? const SizedBox.shrink()  
-          : Padding(  
-              padding: EdgeInsets.only(  
-                  top: navBarEssentials.padding.top,  
-                  bottom: navBarEssentials.padding.bottom),  
-              child: Stack(  
-                alignment: Alignment.center,  
-                children: <Widget>[  
-                  Container(  
-                    width: 150,  
-                    height: height,  
-                    margin: const EdgeInsets.only(top: 2),  
-                    decoration: BoxDecoration(  
-                      shape: BoxShape.circle,  
-                      color: item.activeColorPrimary,  
-                      border: Border.all(color: Colors.transparent, width: 5),  
-                    ),  
-                    child: Container(  
-                      alignment: Alignment.center,  
-                      height: height,  
-                      child: Column(  
-                        mainAxisAlignment: MainAxisAlignment.center,  
-                        children: <Widget>[  
-                          Expanded(  
-                            child: IconTheme(  
-                              data: IconThemeData(  
-                                  size: item.iconSize,  
-                                  color: item.activeColorSecondary ??  
-                                      item.activeColorPrimary),  
-                              child: isSelected  
-                                  ? item.icon  
-                                  : item.inactiveIcon ?? item.icon,  
-                            ),  
-                          ),  
-                        ],  
-                      ),  
-                    ),  
-                  ),  
-                  if (item.title == null)  
-                    const SizedBox.shrink()  
-                  else  
-                    Align(  
-                      alignment: Alignment.bottomCenter,  
-                      child: Material(  
-                        type: MaterialType.transparency,  
-                        child: FittedBox(  
-                            child: Text(  
-                          item.title!,  
-                          style: item.textStyle != null  
-                              ? (item.textStyle!.apply(  
-                                  color: isSelected  
-                                      ? (item.activeColorSecondary ??  
-                                          item.activeColorPrimary)  
-                                      : item.inactiveColorPrimary))  
-                              : TextStyle(  
-                                  color: isSelected  
-                                      ? (item.activeColorPrimary)  
-                                      : item.inactiveColorPrimary,  
-                                  fontWeight: FontWeight.w400,  
-                                  fontSize: 12),  
-                        )),  
-                      ),  
-                    )  
-                ],  
-              ),  
-            );  
+  Widget _buildMiddleItem(final PersistentBottomNavBarItem item,
+          final bool isSelected, final double? height) =>
+      navBarEssentials.navBarHeight == 0
+          ? const SizedBox.shrink()
+          : Padding(
+              padding: EdgeInsets.only(
+                  top: navBarEssentials.padding.top,
+                  bottom: navBarEssentials.padding.bottom),
+              child: Stack(
+                children: <Widget>[
+                  Transform.translate(
+                    offset: const Offset(0, -23),
+                    child: Center(
+                      child: Container(
+                        width: 150,
+                        height: height,
+                        margin: const EdgeInsets.only(top: 2),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: item.activeColorPrimary,
+                          border:
+                              Border.all(color: Colors.transparent, width: 5),
+                        ),
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: height,
+                          child: ListView(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            scrollDirection: Axis.horizontal,
+                            children: <Widget>[
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Expanded(
+                                    child: IconTheme(
+                                      data: IconThemeData(
+                                          size: item.iconSize,
+                                          color: item.activeColorSecondary ??
+                                              item.activeColorPrimary),
+                                      child: isSelected
+                                          ? item.icon
+                                          : item.inactiveIcon ?? item.icon,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  if (item.title == null)
+                    const SizedBox.shrink()
+                  else
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Material(
+                        type: MaterialType.transparency,
+                        child: FittedBox(
+                            child: Text(
+                          item.title!,
+                          style: item.textStyle != null
+                              ? (item.textStyle!.apply(
+                                  color: isSelected
+                                      ? (item.activeColorSecondary ??
+                                          item.activeColorPrimary)
+                                      : item.inactiveColorPrimary))
+                              : TextStyle(
+                                  color: isSelected
+                                      ? (item.activeColorPrimary)
+                                      : item.inactiveColorPrimary,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12),
+                        )),
+                      ),
+                    )
+                ],
+              ),
+            );
   
   @override  
   Widget build(final BuildContext context) {  
